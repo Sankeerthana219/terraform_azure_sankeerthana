b5ce2809d37b985c70f46f5ce532fa648949a7fc
Sankeerthana219 <sankeerthanakolakani219@gmail.com>
2025-05-25 09:14:10 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "azurerm" {
     resource_group_name  = "terraformdemo"
     storage_account_name = "skterraformdemostor990"
-    container_name       = "terraformcontain"
+    container_name       = "skterraformcontain"
     key                  = "default.terraform.tfstate"
     use_oidc             = true
   }
